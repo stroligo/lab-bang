12247dff57c5966b6066efcc8e39faf1b06a537e
stroligo <gabrielstroligo@gmail.com>
2022-10-30 01:58:28 +0200

    tailwind instalado

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,less}",
+    "./src/*.{html,js,less}",
+    "./build/*.{html,js,less}",
+    "./*",
+  ],
   theme: {
     extend: {},
   },
