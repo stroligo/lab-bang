4aedcd31c9bc4784ec1c51f2fb456b25dbfcdd6b
stroligo <gabrielstroligo@gmail.com>
2022-11-03 03:33:07 +0100

    arrumando hand

---
+++ src/scripts/gabriel.js
@@ -0,0 +1,9 @@
+// IMPRIMIR NO CONSOLE GAME
+
+const consoleGame = document.getElementById("console");
+consoleGame.innerHTML += " Fulano Atacou Ciclano \n";
+consoleGame.innerHTML += " Fulano Atacou Ciclano \n";
+consoleGame.innerHTML += " Gabriel Atacou Ciclano \n";
+consoleGame.innerHTML += " Gabriel Atacou Ciclano \n";
+consoleGame.innerHTML += " Gabriel Atacou Ciclano \n";
+consoleGame.innerHTML += " Fulano Atacou Ciclano \n";
