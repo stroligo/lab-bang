6e4b619d1e2d8e40e39834a485150496c9328554
stroligo <gabrielstroligo@gmail.com>
2022-11-04 15:00:33 +0100

    arrumando modal

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,12 +1,13 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var button = document.getElementsByClassName("fechar")[0];
 
+console.log(p1);
+
+console.log(p1);
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = "block";
@@ -24,7 +25,17 @@ window.onclick = function (event) {
   }
 };
 
-/* #player4 .infos .name
-#player4 .infos .name
-#player4 .infos .name
-#player4 .infos .name */
+/* var p1 = document.querySelector("#player1 .infos .name");
+var p2 = document.querySelector("#player2 .infos .name");
+var p3 = document.querySelector("#player3 .infos .name");
+var p4 = document.querySelector("#player4 .infos .name");
+
+var i1 = document.querySelector("#inputName1");
+var i2 = document.querySelector("#inputName1");
+var i3 = document.querySelector("#inputName1");
+var i4 = document.querySelector("#inputName1");
+
+p1.innerHTML = i1.innerHTML;
+p2.innerHTML = i2.innerHTML;
+p2.innerHTML = i3.innerHTML;
+p2.innerHTML = i4.innerHTML; */
